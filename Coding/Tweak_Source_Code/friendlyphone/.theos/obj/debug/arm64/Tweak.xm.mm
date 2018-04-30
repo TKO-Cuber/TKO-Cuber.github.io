89158a4adaf69d747330a3a43c652be6ce0b0e33
TKO-Cuber <tko.cuber@gmail.com>
2018-04-29 21:31:47 -0400

    add work in progress iDeb

---
+++ Coding/Tweak_Source_Code/friendlyphone/.theos/obj/debug/arm64/Tweak.xm.mm
@@ -0,0 +1,52 @@
+#line 1 "Tweak.xm"
+#import <sys/utsname.h>
+
+NSString* deviceName()
+{
+    struct utsname systemInfo;
+    uname(&systemInfo);
+
+    return [NSString stringWithCString:systemInfo.machine
+                              encoding:NSUTF8StringEncoding];
+}
+
+
+#include <substrate.h>
+#if defined(__clang__)
+#if __has_feature(objc_arc)
+#define _LOGOS_SELF_TYPE_NORMAL __unsafe_unretained
+#define _LOGOS_SELF_TYPE_INIT __attribute__((ns_consumed))
+#define _LOGOS_SELF_CONST const
+#define _LOGOS_RETURN_RETAINED __attribute__((ns_returns_retained))
+#else
+#define _LOGOS_SELF_TYPE_NORMAL
+#define _LOGOS_SELF_TYPE_INIT
+#define _LOGOS_SELF_CONST
+#define _LOGOS_RETURN_RETAINED
+#endif
+#else
+#define _LOGOS_SELF_TYPE_NORMAL
+#define _LOGOS_SELF_TYPE_INIT
+#define _LOGOS_SELF_CONST
+#define _LOGOS_RETURN_RETAINED
+#endif
+
+@class SpringBoard; 
+static void (*_logos_orig$_ungrouped$SpringBoard$applicationDidFinishLaunching$)(_LOGOS_SELF_TYPE_NORMAL SpringBoard* _LOGOS_SELF_CONST, SEL, id); static void _logos_method$_ungrouped$SpringBoard$applicationDidFinishLaunching$(_LOGOS_SELF_TYPE_NORMAL SpringBoard* _LOGOS_SELF_CONST, SEL, id); 
+
+#line 12 "Tweak.xm"
+ 
+        static void _logos_method$_ungrouped$SpringBoard$applicationDidFinishLaunching$(_LOGOS_SELF_TYPE_NORMAL SpringBoard* _LOGOS_SELF_CONST __unused self, SEL __unused _cmd, id application) { 
+            _logos_orig$_ungrouped$SpringBoard$applicationDidFinishLaunching$(self, _cmd, application); 
+            UIAlertView *alert1 = [[UIAlertView alloc] initWithTitle:@"Welcome Back!" 
+                message:@"Welcome back to your iPhone"
+                delegate:self
+                cancelButtonTitle:@"Thanks"
+                otherButtonTitles:nil];
+            [alert1 show]; 
+            [alert1 release]; 
+          }
+      
+static __attribute__((constructor)) void _logosLocalInit() {
+{Class _logos_class$_ungrouped$SpringBoard = objc_getClass("SpringBoard"); MSHookMessageEx(_logos_class$_ungrouped$SpringBoard, @selector(applicationDidFinishLaunching:), (IMP)&_logos_method$_ungrouped$SpringBoard$applicationDidFinishLaunching$, (IMP*)&_logos_orig$_ungrouped$SpringBoard$applicationDidFinishLaunching$);} }
+#line 24 "Tweak.xm"
